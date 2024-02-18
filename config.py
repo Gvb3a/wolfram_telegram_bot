b23af33fb4969c92cdef5a2707cf799f88612110
Boris <135196754+Gvb3a@users.noreply.github.com>
2024-02-18 21:32:48 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,9 @@
+bot_token = 'YOU_BOT_TOKEN' # https://t.me/BotFather
+
+spoken_api = 'SPOKEN_API'  # https://products.wolframalpha.com/spoken-results-api/documentation
+simple_api = 'SIMPLE_API'  # https://products.wolframalpha.com/simple-api/documentation
+show_steps_api = 'SHOW_STEPS_API'  # https://products.wolframalpha.com/show-steps-api/documentation
+llm_api = 'LLM_API'  # https://products.wolframalpha.com/llm-api/documentation
+
+# spoken_api = simple_api; show_steps_api = llm_api
+# https://developer.wolframalpha.com/
