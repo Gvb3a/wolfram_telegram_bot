41ac532654e422268c10df75a9619174e5a9e6c1
Boris <135196754+Gvb3a@users.noreply.github.com>
2024-02-26 20:17:44 +0300

    Removing the code

--- streamlit_site.py
+++ streamlit_site.py
@@ -1,22 +1,12 @@
 import streamlit as st
-
 from colorama import Fore, Style, init
-
 init()
 
-def streamlit_command(command, name, current_time):
-    st.write(f':red[{command}] command from :blue[{name}] at {current_time}')
-    print(f'{Fore.RED}{command}{Style.RESET_ALL} command from {Fore.BLUE}{name}{Style.RESET_ALL} at {current_time}')
-
-
-def streamlit_launch(current_time):
-    st.write(f'The bot :red[launches] at {current_time}')
-    print(f'The bot {Fore.RED}launches{Style.RESET_ALL} at {current_time}')
 
 def streamlit_message_input(text, name, mode, time):
-
     st.write(f'Request :green[{text}] from :blue[{name}] with {mode} at {time}')
     print(f'Request {Fore.GREEN}{text}{Style.RESET_ALL} from {Fore.BLUE}{name}{Style.RESET_ALL} with {mode} at {time}')
+
 
 def streamlit_message_output(text, name, time):
     st.write(f'A reply to the :green[{text}] was sent to :blue[{name}] at {time}')
