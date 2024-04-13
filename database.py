9274d6db0b4a53d266e00829f437954cea348aa6
Boris <135196754+Gvb3a@users.noreply.github.com>
2024-04-13 12:13:31 +0300

    Update database.py

--- database.py
+++ database.py
@@ -1,7 +1,7 @@
 import sqlite3  # library for working with the database
-from colorama import init, Fore, Style  # library for colouring text in print
+# from colorama import init, Fore, Style  # library for colouring text in print
 from datetime import datetime  # library for recognising the current time
-init()  # is used to colour text in the cmd
+# init()  # is used to colour text in the cmd
 
 
 def sql_launch():
@@ -9,7 +9,7 @@ def sql_launch():
     cursor = connection.cursor()  # it is necessary to execute queries to the database
     # create tables, if they did not exist before (new file). This table will store information about the message
     cursor.execute('''
-        CREATE TABLE IF NOT EXISTS frame (
+        CREATE TABLE IF NOT EXISTS message (
         message TEXT,
         username TEXT,
         time TEXT,
@@ -19,7 +19,7 @@ def sql_launch():
         ''')
     # You could say that this is a record of what is output to the console (for the site)
     cursor.execute(''' 
-        CREATE TABLE IF NOT EXISTS text (
+        CREATE TABLE IF NOT EXISTS console (
         text TEXT,
         launch_or_not INT
         )
@@ -34,8 +34,9 @@ def sql_launch():
         ''')
     # output information about successful startup to the console and save this information to the database
     current_time = datetime.now().strftime("%H:%M:%S %d.%m.%Y")
-    print(f'The bot {Fore.RED}launches{Style.RESET_ALL} at {current_time}')
-    cursor.execute(f"INSERT INTO text(text, launch_or_not) VALUES ('The bot :red[launches] at {current_time}', 1)")
+    print(f'The bot launches at {current_time}')
+    # print(f"The bot {Fore.RED}launches{Style.RESET_ALL} at {current_time}")
+    cursor.execute(f"INSERT INTO console(text, launch_or_not) VALUES ('The bot :red[launches] at {current_time}', 1)")
 
     connection.commit()  # Save the changes to the database
     connection.close()  # close the database
@@ -50,25 +51,25 @@ def sql_message(message, name, user_id, add):
     if row is not None and row[0] is not None:
         if not (name in row[0]):
             cursor.execute(f"UPDATE user SET name = '{row[0]}, {name}' WHERE id = {user_id}")
-            add += '(update name)'
+            add += 'Update name'
     else:
         cursor.execute(f"INSERT INTO user(name, id, mode) VALUES ('{name}', {user_id}, 1)")
-        add += '(new user)'
+        add += 'New user'
 
     if message == '/mode' and len(message) == 5:
         if row[2]:
             cursor.execute(f"UPDATE user SET mode = 0 WHERE id = {user_id}")
         else:
             cursor.execute(f"UPDATE user SET mode = 1 WHERE id = {user_id}")
-        message += f'({not(row[2])})'
+        message += str(not(row[2]))
 
     current_time = datetime.now().strftime("%H:%M:%S %d.%m.%Y")
-
-    print(f'{Fore.RED}{message}{Style.RESET_ALL} from {Fore.BLUE}{name}({user_id}){Style.RESET_ALL} at {current_time}. {add}')
+    print(f'{message} from {name} at {current_time}. {add}')
+    # print(f'{Fore.RED}{message}{Style.RESET_ALL} from {Fore.BLUE}{name}({user_id}){Style.RESET_ALL} at {current_time}. {add}')
     cursor.execute(
-        f"INSERT INTO frame(message, username, time, id, additionally) VALUES ('{message}', '{name}', '{current_time}', {user_id}, '{add}')")
+        f"INSERT INTO message(message, username, time, id, additionally) VALUES ('{message}', '{name}', '{current_time}', {user_id}, '{add}')")
     cursor.execute(
-        f"INSERT INTO text(text, launch_or_not) VALUES (':red[{message}] from :blue[{name}({user_id})] at {current_time}. {add}', 0)")
+        f"INSERT INTO console(text, launch_or_not) VALUES (':red[{message}] from :blue[{name}] at {current_time}. {add}', 0)")
 
     connection.commit()
     connection.close()
