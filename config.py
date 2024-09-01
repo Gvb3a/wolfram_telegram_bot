ab39cd14abb68ffcd8ca46285865937a858d6564
Boris <135196754+Gvb3a@users.noreply.github.com>
2024-09-01 19:13:22 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,14 @@
+BOT_TOKEN=''
+SIMPLE_API=''
+SHOW_STEP_API=''
+SIMPLE_TEX_API=''
+DETECT_LANGUAGE_API=''
+GROQ_API=''
+
+prompt = """You will need to turn the user's request into a correct one for wolfram alpha (you get the ones that WolframAlpha doesn't understand).
+
+Answer in the following format:
+
+Thought: Think about what the user wanted to write or get Action 
+Action Input: What goes into WolframAlpha
+"""
